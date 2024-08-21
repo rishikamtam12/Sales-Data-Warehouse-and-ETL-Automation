50e219d9972bb5f7ac4582bdca72bd72eeb176b2
Rishi Kamtam <157946455+rishikamtam12@users.noreply.github.com>
2024-08-21 15:52:43 -0400

    Add files via upload

---
+++ CreateStarSchema.KamtamR.R
@@ -0,0 +1,194 @@
+# ---------------------------------------
+# Title: Create Star Schema
+# Subtitle: CS3200 / Final Project
+# Author: Rishi Kamtam
+# Date: Summer 2 2024
+# ---------------------------------------
+
+
+# loading the necessary libaries
+library(RMySQL)
+
+# defining parameters for MySQL database connection
+db_name <- "sql5726266"
+db_user <- "sql5726266"
+db_host <- "sql5.freesqldatabase.com"
+db_pwd <- "YeQH7DryPF"
+db_port <- 3306
+
+# establishing connection to MySQL database
+mysql_dbcon <-  dbConnect(RMySQL::MySQL(), user = db_user, password = db_pwd,
+                      dbname = db_name, host = db_host, port = db_port)
+
+# connecting to SQLite database
+sqlite_dbcon <- dbConnect(RSQLite::SQLite(), "final_project.db")
+
+
+# dropping the tables so code can be re run 
+dbExecute(mysql_dbcon, "DROP TABLE IF EXISTS product_facts")
+dbExecute(mysql_dbcon, "DROP TABLE IF EXISTS time_dim")
+dbExecute(mysql_dbcon, "DROP TABLE IF EXISTS location_dim")
+
+
+
+###
+### Creating fact and dimension tables
+###
+
+# creating product fact table
+create_prod_facts_sql <- "
+CREATE TABLE IF NOT EXISTS product_facts(
+  fact_id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  product_name TEXT NOT NULL,
+  total_amount REAL NOT NULL,
+  total_units INTEGER NOT NULL,
+  time_id INTEGER NOT NULL,
+  location_id INTEGER NOT NULL,
+  FOREIGN KEY (time_id) REFERENCES time_dim(time_id),
+  FOREIGN KEY (location_id) REFERENCES location_dim(location_id)
+    );"
+
+# creating time dimension table
+create_time_dim_sql <- "
+CREATE TABLE IF NOT EXISTS time_dim(
+  time_id INT PRIMARY KEY AUTO_INCREMENT,
+  year INT NOT NULL,
+  month INT NOT NULL
+);"
+
+# creating location dimension table
+create_location_dim_sql <- "
+CREATE TABLE IF NOT EXISTS location_dim(
+  location_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  location_name TEXT NOT NULL
+);"
+
+# executing creation of the tables
+dbExecute(mysql_dbcon, create_time_dim_sql)
+dbExecute(mysql_dbcon, create_location_dim_sql)
+dbExecute(mysql_dbcon, create_prod_facts_sql)
+
+
+###
+### Populating data into dimension tables
+###
+
+# getting a list of the sales tables for each year from sqlite db
+sales_tables <- dbGetQuery(sqlite_dbcon, "
+  SELECT name FROM sqlite_master 
+  WHERE type='table' AND name LIKE 'sales_%';
+")$name
+
+
+# creating empty data frame for time_dim table
+time_data <- data.frame()
+
+# looping through sales_tables and getting the data for time_dim table
+for (sales_table in sales_tables) {
+  # querying needed data for time_dim table
+  query <- paste0("
+    SELECT DISTINCT CAST(strftime('%Y', date) AS INTEGER) AS year, 
+                    CAST(strftime('%m', date) AS INTEGER) AS month
+    FROM ", sales_table, "
+    ORDER BY year, month;
+  ")
+  # adding queried data to data frame
+  time_data <- rbind(time_data, dbGetQuery(sqlite_dbcon, query))
+}
+
+# inserting data into time dim table
+dbWriteTable(mysql_dbcon, "time_dim", time_data, append = TRUE,
+             row.names = FALSE)
+
+
+
+
+# creating empty data frame for location_dim table
+location_data <- data.frame()
+
+# looping through sales_tables and getting the data for location_dim table
+for (sales_table in sales_tables) {
+  # querying needed data for location_dim table
+  query <- paste0("
+    SELECT DISTINCT country AS location_name
+    FROM ", sales_table, " 
+    JOIN customers ON ", sales_table, ".custID = customers.custID
+    ORDER BY location_name;
+  ")
+  
+  # adding queried data to data frame
+  location_data <- rbind(location_data, dbGetQuery(sqlite_dbcon, query))
+}
+
+# removing duplicate location names 
+location_data <- unique(location_data)
+
+# inserting data into location dim table
+dbWriteTable(mysql_dbcon, "location_dim", location_data, append = TRUE,
+             row.names = FALSE)
+
+
+
+###
+### Populating data into fact table
+###
+
+# looping through sales_tables and getting data for product_facts table
+for (sales_table in sales_tables) {
+  # querying and aggregating needed data for product_facts table
+  query <- paste0("
+    SELECT 
+      p.prod AS product_name,
+      SUM(s.qty * s.unitcost) AS total_amount,
+      SUM(s.qty) AS total_units,
+      strftime('%Y', s.date) AS year,
+      strftime('%m', s.date) AS month,
+      c.country AS location_name
+    FROM ", sales_table, " s
+    JOIN customers c ON s.custID = c.custID
+    JOIN products p ON s.prodID = p.prodID
+    GROUP BY product_name, year, month, location_name;
+  ")
+  
+  # running and storing the query
+  product_facts_data <- dbGetQuery(sqlite_dbcon, query)
+  
+  # converting month to integer to match time_dim
+  product_facts_data$month <- as.integer(product_facts_data$month) 
+  
+  
+  # merging product_facts and time_dim to add the time_id based on yr and month
+  time_dim <- dbGetQuery(mysql_dbcon, "SELECT * FROM time_dim")
+  product_facts_data <- merge(
+    product_facts_data, 
+    time_dim, 
+    by = c("year", "month")
+  )
+  
+  # merging product_facts and location_dim to add the location_id based on name
+  location_dim <- dbGetQuery(mysql_dbcon, "SELECT * FROM location_dim")
+  product_facts_data <- merge(
+    product_facts_data, 
+    location_dim, 
+    by = "location_name"
+  )
+  
+  # selecting the columns needed for the fact table
+  product_facts_data <- product_facts_data[, c("product_name", "total_amount",
+                                      "total_units", "time_id", "location_id")]
+  
+  # ordering the data by time_id and location_id
+  product_facts_data <- product_facts_data[order(product_facts_data$time_id,
+                                            product_facts_data$location_id), ]
+  
+  # inserting the data into the product_facts table
+  dbWriteTable(mysql_dbcon, "product_facts", product_facts_data, append = TRUE,
+               row.names = FALSE)
+}
+
+
+
+# disconnecting from databases
+dbDisconnect(mysql_dbcon)
+dbDisconnect(sqlite_dbcon)
+  
---
+++ LoadXML2DB.KamtamR.R
@@ -0,0 +1,194 @@
+# ---------------------------------------
+# Title: Create Analytics Database
+# Subtitle: CS3200 / Final Project
+# Author: Rishi Kamtam
+# Date: Summer 2 2024
+# ---------------------------------------
+
+
+# Urls to sources I used for this assignment:
+# https://cran.r-project.org/web/packages/data.table/vignettes/datatable-intro.html
+# https://www.machinelearningplus.com/data-manipulation/datatable-in-r-complete-guide/
+
+
+# ERD for relational schema:
+# https://lucid.app/lucidchart/28c476fe-c0df-4f4b-8ca9-17789a423747/edit?viewport_loc=-99%2C-25%2C1996%2C1033%2C0_0&invitationId=inv_4b373cf3-de2c-42b6-a1c5-17a6ddeed9ca
+
+
+
+# loading necessary libraries
+library(RSQLite)
+library(data.table)
+
+# connecting to database
+dbcon <- dbConnect(RSQLite::SQLite(), "final_project.db")
+
+# enabling foreign key constraints 
+dbExecute(dbcon, "PRAGMA foreign_keys = ON")
+
+
+# dropping the tables so code can be re run 
+dbExecute(dbcon, "DROP TABLE IF EXISTS sales_2020")
+dbExecute(dbcon, "DROP TABLE IF EXISTS sales_2021")
+dbExecute(dbcon, "DROP TABLE IF EXISTS sales_2022")
+dbExecute(dbcon, "DROP TABLE IF EXISTS sales_2023")
+dbExecute(dbcon, "DROP TABLE IF EXISTS sales")
+dbExecute(dbcon, "DROP TABLE IF EXISTS reps")
+dbExecute(dbcon, "DROP TABLE IF EXISTS products")
+dbExecute(dbcon, "DROP TABLE IF EXISTS customers")
+
+
+###
+### creating tables
+###
+
+# creating products table
+create_prod_sql <- "
+CREATE TABLE IF NOT EXISTS products (
+  prodID INTEGER NOT NULL,
+  prod TEXT NOT NULL,
+  PRIMARY KEY (prodID)
+  );"
+
+# creating reps table
+create_reps_sql <- "
+CREATE TABLE IF NOT EXISTS reps (
+  repID INTEGER NOT NULL,
+  repFN TEXT NOT NULL,
+  repLN TEXT NOT NULL,
+  repTR TEXT NOT NULL,
+  repPh TEXT NOT NULL,
+  repCm REAL NOT NULL,
+  repHireDate DATE,
+  PRIMARY KEY (repID)
+  );"
+
+# creating customers table
+create_customers_sql <- "
+CREATE TABLE IF NOT EXISTS customers(
+  custID INTEGER NOT NULL,
+  cust TEXT NOT NULL,
+  country TEXT NOT NULL,
+  PRIMARY KEY(custID)
+  );"
+
+# creating sales table
+create_sales_sql <- "
+CREATE TABLE IF NOT EXISTS sales(
+  salesID INTEGER NOT NULL,
+  date DATE NOT NULL,
+  qty INTEGER NOT NULL,
+  unitcost REAL NOT NULL,
+  custID INTEGER NOT NULL,
+  prodID INTEGER NOT NULL,
+  repID INTEGER NOT NULL,
+  FOREIGN KEY (custID) REFERENCES customers(custID),
+  FOREIGN KEY (prodID) REFERENCES products(prodID),
+  FOREIGN KEY (repID) REFERENCES reps(repID)
+  );"
+
+
+# executing the creation of all the tables
+dbExecute(dbcon, create_prod_sql)
+dbExecute(dbcon, create_reps_sql)
+dbExecute(dbcon, create_customers_sql)
+dbExecute(dbcon, create_sales_sql)
+
+
+
+
+###
+### extracting, transforming, and loading the data into the database
+### 
+### Note: Converting dates to YYYY-MM-DD format as it is widely supported
+### Planning on converting dates while handling the data
+###
+
+# creating a list of files with same naming pattern
+rep_files <- list.files(path = "csv-data", pattern = "pharmaReps.*\\.csv$",
+                        full.names = TRUE)
+txn_files <- list.files(path = "csv-data", pattern = "pharmaSalesTxn.*\\.csv$",
+                        full.names = TRUE)
+
+
+# reading and combining (using rbindlist) all rep files into a single data.table
+rep_data_list <- lapply(rep_files, fread)
+reps_dt <- rbindlist(rep_data_list)
+
+# converting the rep hire date from to YYYY-MM-DD format
+# := operater used to update/create a column in a data table
+reps_dt[, repHireDate := format(as.Date(repHireDate, format = "%b %d %Y"),
+                                "%Y-%m-%d")]
+
+# reading and combining all transaction files into a single data.table
+txn_data_list <- lapply(txn_files, fread)
+txn_dt <- rbindlist(txn_data_list)
+
+# extracting unique customers from transaction data and assigning a customer ID 
+customers_dt <- txn_dt[, .(cust, country)]
+customers_dt <- unique(customers_dt, by = "cust")
+customers_dt[, custID := .I]
+
+# extracting unique products from transaction data and assigning a product ID
+products_dt <- txn_dt[, .(prod)]
+products_dt <- unique(products_dt, by = "prod")
+products_dt[, prodID := .I]
+
+
+# merging txn_dt with customers_dt and products_dt to add custID and prodID
+txn_dt <- merge(txn_dt, customers_dt[, .(cust, custID)], by = "cust",
+                all.x = TRUE)
+txn_dt <- merge(txn_dt, products_dt[, .(prod, prodID)], by = "prod",
+                all.x = TRUE)
+
+
+# generating sales_id for each transaction and converting date to new format
+sales_dt <- txn_dt[, .(date = format(as.Date(date, format = "%m/%d/%Y"),
+                                     "%Y-%m-%d"),
+                       qty, unitcost, custID, prodID, repID)]
+sales_dt[, salesID := .I]
+
+
+# writing data to the respective tables
+dbWriteTable(dbcon, "reps", reps_dt, append = TRUE, row.names = FALSE)
+dbWriteTable(dbcon, "customers", customers_dt, append = TRUE, row.names = FALSE)
+dbWriteTable(dbcon, "products", products_dt, append = TRUE, row.names = FALSE)
+dbWriteTable(dbcon, "sales", sales_dt, append = TRUE, row.names = FALSE)
+
+
+###
+###
+### Date range partitioning on sales table
+###
+###
+
+# extracting the years found in the sales table based on date column
+years <- dbGetQuery(dbcon, "SELECT DISTINCT strftime('%Y', sales.date) AS year 
+                    FROM sales ORDER BY year;")
+# converting the years into a list for easier iteration
+years <- as.list(years$year)
+
+# looping through the list of years to create a new table for each of them
+for (year in years) {
+  # creating the name of the new table for each year
+  table_name <- paste0("sales_", year)
+
+  # creating the new table for the given year
+  # query within create table statement populates tables based on given year
+  dbExecute(dbcon, paste0("
+    CREATE TABLE IF NOT EXISTS ", table_name, " AS
+    SELECT * FROM sales WHERE strftime('%Y', date) = '", year, "';
+  "))
+}
+
+
+# removing large sales table 
+dbExecute(dbcon, "DROP TABLE IF EXISTS sales;")
+
+
+# disconnecting from database
+dbDisconnect(dbcon)
+
+
+
+
